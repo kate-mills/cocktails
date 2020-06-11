b8f78317dff92b83fbe95edf6b1efaf45462194a
Kate Mills <kate.mills1124@gmail.com>
2020-06-11 16:29:42 -0700

    add class to cocktail list view

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -16,7 +16,7 @@ const CocktailList = ({loading, cocktails}) => {
     return(
       <section className="section">
         <h2 className="section-title">cocktails</h2>
-        <div className="cocktails-array">
+        <div className="cocktails-center">
           {cocktails.map( item => {
             return (
               <Cocktail key={item.id} {...item} />
