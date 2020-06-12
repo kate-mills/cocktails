84bfc58f21d0d3126bdb5efecfca3127185b4dc0
Kate Mills <kate.mills1124@gmail.com>
2020-06-12 13:43:38 -0700

    SearchForm added

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,42 @@
 import React from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+const SearchForm = ({setSearchTerm}) => {
+
+  // Holds value between renders.
+  const searchValue = React.useRef('');
+
+  // focus form input  on render
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, []);
+  
+  // No page refresh
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  // Set value while user types input
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">search cocktails</h2>
+      <form className="form search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input 
+            type="text"
+            name="name"
+            id="name"
+            onChange={searchCocktail}
+            ref={searchValue}
+          />
+        </div>
+      </form>
+    </section>
+  )
 }
+
+export default SearchForm
